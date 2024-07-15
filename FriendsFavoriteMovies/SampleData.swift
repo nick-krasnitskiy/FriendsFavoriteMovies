8d661f6613158eccc1c1d4be70fa2db5dd62fe59
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2024-07-15 13:21:47 +0300

    Use a class to provide sample data.

---
+++ FriendsFavoriteMovies/SampleData.swift
@@ -0,0 +1,33 @@
+//
+//  SampleData.swift
+//  FriendsFavoriteMovies
+//
+//  Created by Nick Krasnitskiy on 15.07.2024.
+//
+
+import Foundation
+import SwiftData
+
+@MainActor
+class SampleData {
+    static let shared = SampleData()
+    
+    let modelContainer: ModelContainer
+    
+    var context: ModelContext {
+        modelContainer.mainContext
+    }
+    
+    private init() {
+        let schema = Schema([
+            Movie.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: true)
+
+        do {
+            modelContainer = try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }
+}
